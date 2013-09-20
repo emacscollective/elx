91e4cda6b4119a7826d6b16c254c8d02376e6add
Jonas Bernoulli <jonas@bernoul.li>
2013-09-20 03:45:30 +0200

    in Commentary mention that there are also additions

--- elx.el
+++ elx.el
@@ -26,9 +26,13 @@
 ;;; Commentary:
 
 ;; This package extracts information from Emacs Lisp libraries.  It
-;; extends `lisp-mnt', which is only suitable for libraries that closely
-;; follow the header conventions.  Unfortunately there are many libraries
-;; that do not - this library tries to cope with that.
+;; extends built-in `lisp-mnt', which is only suitable for libraries
+;; that closely follow the header conventions.  Unfortunately there
+;; are many libraries that do not - this library tries to cope with
+;; that.
+
+;; It also defines some new extractors not available in `lisp-mnt',
+;; and some generalizations of extractors available in the latter.
 
 ;;; Code:
 
